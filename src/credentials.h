2107e0c30ae13634e3b843730a1747feec8b41c7
Sergio Oviedo <soviedos@gmail.com>
2021-03-21 12:56:25 -0600

    Added Dani's subscription

--- src/credentials.h
+++ src/credentials.h
@@ -1,8 +1,24 @@
+/**
+ * @Author: Sergio Oviedo Seas
+ * @Date:   2021-03-06 15:23:13
+ * @Last Modified by:   Sergio Oviedo Seas
+ * @Last Modified time: 2021-03-21 12:55:12
+ */
 // Fill in  your WiFi networks SSID and password
 #define SECRET_SSID      "TNT3"
 #define SECRET_PASS      "DFGHMNBVZX"
 
 // Fill in the hostname of your Azure IoT Hub broker
-#define SECRET_BROKER    "NTicSolution.azure-devices.net"
-#define AZURE_PASS "SharedAccessSignature sr=NTicSolution.azure-devices.net%2Fdevices%2FM0&sig=MUXmAHSr2Q0gGSd7eZYtdiBzt5xQZTDjhhHEzBCjvAA%3D&se=1644550052"
-#define SECRET_DEVICE_ID "M0"
+//#define SECRET_BROKER    "NTicSolution.azure-devices.net"
+
+// DANI'S SECRET_BROKER
+#define SECRET_BROKER "ntic-iothub.azure-devices.net"
+
+// DANI'S SAS TOKEN
+#define AZURE_PASS "SharedAccessSignature sr=ntic-iothub.azure-devices.net%2Fdevices%2Fmy-iot-device&sig=X537jxZ05AC0nZXpsbLMotSCdYBjiXKgNAITt0lAKiY%3D&se=1648474573"
+
+//#define AZURE_PASS "SharedAccessSignature sr=NTicSolution.azure-devices.net%2Fdevices%2FM0&sig=MUXmAHSr2Q0gGSd7eZYtdiBzt5xQZTDjhhHEzBCjvAA%3D&se=1644550052"
+
+//DANI'S DEVICE ID
+#define SECRET_DEVICE_ID "my-iot-device"
+//#define SECRET_DEVICE_ID "M0"
